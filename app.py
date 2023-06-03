016f6c1e77bddb54b1e5759aa9903c06a4846d35
OliverH144 <s_hartmanno20@stud.hwr-berlin.de>
2023-06-03 16:24:22 +0200

    adding db test stuff

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask, render_template, redirect, url_for, request
 import db
+import random
 
 app = Flask(__name__)
 
@@ -23,6 +24,33 @@ def get_quiz():
 @app.route('/quiz2/')
 def get_quiz2():
     return render_template('quiz2.html')
+
+#testing some db interaction
+@app.route('/db/')
+def get_questions():
+    db_con = db.get_db_con()
+    sql_query = 'SELECT COUNT(question) from questions'
+    question_count = db_con.execute(sql_query).fetchone()
+    count=question_count[0]
+    i=0
+    random_numbers=[]
+    r_questions=[]
+    while i <10:
+        number = random.randint(1, count)
+        if number not in random_numbers:
+            random_numbers.append(number)
+            i=i+1
+        else: continue
+    j=0
+    while j <10:
+        sql_query1 = f'SELECT question FROM questions WHERE question_id = {random_numbers[j]}'
+        question_query = db_con.execute(sql_query1).fetchone()
+        question = question_query[0]
+        r_questions.append(question)
+        j=j+1
+    #vlt random.shuffle(list) nutzen
+    return f'Count:{count} Random Numbers:{random_numbers} Random Questions:{r_questions}'
+
 
 #@app.route('/answer/', methods=['GET'])  # Notice the 'methods' argument
 #def answer():
